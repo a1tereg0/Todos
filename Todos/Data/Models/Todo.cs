aef2c59b676d043e0d131fa6ac6e7b6e7512010e
Prashant Dahal <prashantdah41@gmail.com>
2021-04-22 17:48:17 -0400

    Create Todo Model with proper data annotations

---
+++ Todos/Data/Models/Todo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todos.Data.Models
+{
+    public class Todo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TodoId { get; set; }
+
+        [StringLength(50, ErrorMessage = "Your title cannot be more than 50 characters")]
+        [Required]
+        public string Title { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        [Display(Name = "E-mail Address")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime DateAdded => DateTime.Now;
+
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime DueDate { get; set; }
+
+        public Boolean Completed { get; set; } = false;
+
+        [DataType(DataType.DateTime)]
+        public DateTime DateCompleted { get; set; }
+    }
+}
