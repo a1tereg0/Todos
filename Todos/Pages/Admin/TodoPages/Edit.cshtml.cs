1116d2d65e1e60eb5752c682e206cfda09373274
Prashant Dahal <prashantdah41@gmail.com>
2021-04-23 16:29:56 -0400

    Add Authorize attributes

--- Todos/Pages/Admin/TodoPages/Edit.cshtml.cs
+++ Todos/Pages/Admin/TodoPages/Edit.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,6 +12,7 @@ using Todos.Data.Models;
 
 namespace Todos.Pages.Admin.TodoPages
 {
+    [Authorize]
     public class EditModel : PageModel
     {
         private readonly Todos.Data.TodoDbContext _context;
