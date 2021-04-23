1c4bf5e1968e4b09f1319e656ac85555be528b73
Prashant Dahal <prashantdah41@gmail.com>
2021-04-23 15:10:46 -0400

    Add service worker and pre-cache index, css and js files

---
+++ Todos/wwwroot/sw.js
@@ -0,0 +1,24 @@
+﻿const DEFAULT_CACHE = "default_cache-v1";
+
+let urls = [
+    "/",
+    "/css/site.css",
+    "/js/site.js"
+];
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(DEFAULT_CACHE)
+            .then(function (cache) {
+                return cache.addAll(urls);
+            })
+    );
+});
+
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((resp) => {
+            return resp || fetch(event.request).then((response) => response);
+        })
+    );
+});
