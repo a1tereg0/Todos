bb709078eaecebf54b653cde9dfc161788bf7021
Prashant Dahal <prashantdah41@gmail.com>
2021-04-23 15:38:27 -0400

    Disable submit button for creating or editing todo when offline

--- Todos/wwwroot/js/site.js
+++ Todos/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+if ('onLine' in navigator) {
+    if (!navigator.onLine) {
+        if (window.location.pathname === '/Admin/TodoPages/Create' || window.location.pathname === '/Admin/TodoPages/Edit') {
+            
+        }
+    }
+}
+
+window.addEventListener('offline', () => {
+    if (window.location.pathname === '/Admin/TodoPages/Create' || window.location.pathname === '/Admin/TodoPages/Edit') {
+        alert("Cant add or make changes when offline");
+        let submitButton = document.querySelector('input[type=submit]')
+        submitButton.disabled = true;
+    }
+})
+window.addEventListener('online', () => {
+    if (window.location.pathname === '/Admin/TodoPages/Create' || window.location.pathname === '/Admin/TodoPages/Edit') {
+        let submitButton = document.querySelector('input[type=submit]')
+        submitButton.disabled = false;
+    }
+})
